64cbfb94db433c91cff46551fdac97b00a331e8f
Andreas Källberg <andreas@svalna.se>
2019-12-04 13:07:04 +0100

    Add options to executable
    Now, two arguments will just save final product and three arguments
    will cause it to trace the execution of the algorithm

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,10 @@ import Lib
 main :: IO ()
 main = do
     args <- getArgs
-    let (from, to, trace) = case args of
-            [f,t, tr] -> (f,t, tr) 
-            _ -> ("img/big-maze.png", "img/big-maze-solved.png", "img/big-maze-trace")
+    case args of
+        [from,to, trace] -> solveTraceMaze from to trace
+        [from,to] -> solveMazeFile from to
+        _ -> solveMazeFile "img/big-maze.png" "img/big-maze-solved.png"
+        -- _ -> ("img/big-maze.png", "img/big-maze-solved.png", "img/big-maze-trace")
     -- solveMazeFile from to
-    solveTraceMaze from to trace
+    -- solveTraceMaze from to trace
